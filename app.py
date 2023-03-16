9433a8679701baa4c0eb5c09c3e5fdbb70b6a3f7
henriits <121551949+henriits@users.noreply.github.com>
2023-03-16 21:09:42 +0200

    finished code

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def bmi():
-    return render_template('bmi.html')
+def index():
+    return render_template('index.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/calculate', methods=['POST'])
 def calculate_bmi():
-    weight = float(request.form['weight'])
-    height = float(request.form['height']) / 100
-    bmi = round(weight / (height ** 2), 2)
-    return render_template('bmi.html', bmi=bmi)
+    height = int(request.form['height'])
+    weight = int(request.form['weight'])
+    bmi = round(weight / ((height / 100) ** 2), 2)
+
+    if bmi < 18.5:
+        bmi_description = "underweight"
+    elif bmi < 25:
+        bmi_description = "normal weight"
+    elif bmi < 30:
+        bmi_description = "overweight"
+    else:
+        bmi_description = "obese"
+
+    return render_template('index.html', bmi=bmi, bmi_description=bmi_description)
 
 if __name__ == '__main__':
     app.run(debug=True)
